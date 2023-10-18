72f8a758859ca48e232144cbdcebc64a3be81aef
jrodcab519 <jrodcab519@g.educaand.es>
2023-10-18 12:22:54 +0200

    versión inicial

--- CrecienteOdecreciente.java
+++ CrecienteOdecreciente.java
@@ -1,10 +1,37 @@
 import java.util.Scanner;
 
 public class CrecienteOdecreciente {
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        Scanner s = new Scanner(System.in);
+        int numeroAnterior = Integer.MAX_VALUE;
+        boolean creciente = true;
+        boolean decreciente = true;
 
+        System.out.println("Introduce una secuencia de números enteros (introduce un valor negativo para finalizar):");
+
+        while (true) {
+            int numero = scanner.nextInt();
+
+            if (numero < 0) {
+                break;
+            }
+
+            if (numero > numeroAnterior) {
+                decreciente = false;
+            } else if (numero < numeroAnterior) {
+                creciente = false;
+            }
+
+            numeroAnterior = numero;
+        }
+
+        if (creciente) {
+            System.out.println("Es creciente.");
+        } else if (decreciente) {
+            System.out.println("Es decreciente.");
+        } else {
+            System.out.println("No es ni creciente ni decreciente.");
+        }
     }
 }
