236088843ff9a6c01cf7a473bc41013f041ee040
jrodcab519 <jrodcab519@g.educaand.es>
2023-10-18 11:08:03 +0200

    versión inicial

--- CuantosAutobuses.java
+++ CuantosAutobuses.java
@@ -6,5 +6,21 @@ public class CuantosAutobuses {
 
         Scanner s = new Scanner(System.in);
 
+        int personasGrupo =0;
+
+        while (true) {
+            int numero = s.nextInt();
+            System.out.println("Personas del grupo (0=FIN): " + numero);
+            if(numero>0){
+            personasGrupo += numero;}
+             else if (numero<0) {
+                System.out.println("No puede ser negativo");}
+             else {
+                 break;
+            }
+            System.out.println("Apuntados. Van " + personasGrupo);
+        }
+        System.out.println("Son necesarios " + personasGrupo/55 + " autobuses");
+
     }
 }
