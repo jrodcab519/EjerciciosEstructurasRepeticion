cf9bd9dc50d9dcc3fe156f98d1843fd4ad94d250
jrodcab519 <jrodcab519@g.educaand.es>
2023-10-19 10:19:31 +0200

    version 1.0

--- CrecienteOdecreciente.java
+++ CrecienteOdecreciente.java
@@ -1,37 +1,42 @@
 import java.util.Scanner;
 
 public class CrecienteOdecreciente {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        int numeroAnterior = Integer.MAX_VALUE;
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int numeroAnterior = 0;
+        int numeroActual = 0;
+        int contador = 1;
         boolean creciente = true;
         boolean decreciente = true;
 
-        System.out.println("Introduce los números que desees (introduce un valor negativo para finalizar):");
-
-        while (true) {
-            int numero = scanner.nextInt();
-
-            if (numero < 0) {
-                break;
+        do {
+            System.out.print(">");
+            numeroActual = s.nextInt();
+            if(contador > 1 && numeroActual >= 0) {
+                if (numeroActual > numeroAnterior) {
+                    decreciente = false;
+                } else if (numeroActual < numeroAnterior) {
+                    creciente = false;
+                } else {
+                    decreciente = false;
+                    creciente = false;
+                }
             }
+            numeroAnterior = numeroActual;
+            contador++;
 
-            if (numero > numeroAnterior) {
-                decreciente = false;
-            } else if (numero < numeroAnterior) {
-                creciente = false;
-            }
+        } while (numeroActual >= 0);
 
-            numeroAnterior = numero;
-        }
-
-        if (creciente) {
-            System.out.println("Es creciente.");
-        } else if (decreciente) {
-            System.out.println("Es decreciente.");
-        } else {
-            System.out.println("No es ni creciente ni decreciente.");
+        if(creciente && decreciente){
+            System.out.println("No se han introducido números");
+        }else if (decreciente && !creciente){
+            System.out.println("Es decreciente");
+        }else if (creciente && !decreciente) {
+            System.out.println("Es creciente");
+        }else if (!creciente && !decreciente){
+            System.out.println("No es creciente ni decreciente");
         }
     }
 }
